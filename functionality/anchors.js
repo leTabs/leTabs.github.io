e7b7482a596b6599846ebeabc415f25223feb287
leTabs <125308756+leTabs@users.noreply.github.com>
2023-07-17 14:30:27 +0300

    Create anchors.js

---
+++ functionality/anchors.js
@@ -0,0 +1,13 @@
+const fbImage= document.querySelector('.fbImg')
+const inImage =document.querySelector('.inImg')
+const gitImage = document.querySelector('.gitImg')
+
+fbImage.addEventListener('click', ()=>{
+    window.location ='https://www.facebook.com/lysimaxos.A/'
+})
+inImage.addEventListener('click', ()=>{
+    window.location ='https://www.linkedin.com/in/lysimaxos-lepesiwths-956651271/'
+})
+gitImage.addEventListener('click', ()=>{
+    window.location = 'https://github.com/leTabs'
+})
