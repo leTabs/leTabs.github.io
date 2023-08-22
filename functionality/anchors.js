30c659530008a4eaaa79f4b3796f8d55b12c041f
leTabs <125308756+leTabs@users.noreply.github.com>
2023-08-22 17:28:25 +0300

    Update anchors.js

--- functionality/anchors.js
+++ functionality/anchors.js
@@ -20,18 +20,42 @@ document.querySelector('.close01').addEventListener('click', ()=>{
     document.querySelector('.first-project').style.display = 'none'
 })
 
-let imgIndex = 1
+let imgIndex01 = 1
 document.querySelector('.next-img').addEventListener('click', ()=>{
-    document.querySelector(`.first-project-img0${imgIndex}`).style.display = 'none'
-    if(imgIndex < 8){
-    imgIndex++}
-    document.querySelector(`.first-project-img0${imgIndex}`).style.display = 'inline'
+    document.querySelector(`.first-project-img0${imgIndex01}`).style.display = 'none'
+    if(imgIndex01 < 8){
+    imgIndex01++}
+    document.querySelector(`.first-project-img0${imgIndex01}`).style.display = 'inline'
 
 })
 document.querySelector('.previous-img').addEventListener('click', ()=>{
-    document.querySelector(`.first-project-img0${imgIndex}`).style.display = 'none'
-    if(imgIndex > 1){
-    imgIndex--}
-    document.querySelector(`.first-project-img0${imgIndex}`).style.display = 'inline'
+    document.querySelector(`.first-project-img0${imgIndex01}`).style.display = 'none'
+    if(imgIndex01 > 1){
+    imgIndex01--}
+    document.querySelector(`.first-project-img0${imgIndex01}`).style.display = 'inline'
+
+})
+
+// second
+
+document.querySelector('.eyeProject02').addEventListener('click', ()=>{
+    document.querySelector('.second-project').style.display = 'block'
+})
+document.querySelector('.close02').addEventListener('click', ()=>{
+    document.querySelector('.second-project').style.display = 'none'
+})
+
+let imgIndex02 = 1
+document.querySelector('.next-img02').addEventListener('click', ()=>{
+    document.querySelector(`.second-project-img0${imgIndex02}`).style.display = 'none'
+    if(imgIndex02 < 13){
+    imgIndex02++}
+    document.querySelector(`.second-project-img0${imgIndex02}`).style.display = 'inline'
+})
+document.querySelector('.previous-img02').addEventListener('click', ()=>{
+    document.querySelector(`.second-project-img0${imgIndex02}`).style.display = 'none'
+    if(imgIndex02 > 1){
+    imgIndex02--}
+    document.querySelector(`.second-project-img0${imgIndex02}`).style.display = 'inline'
 
 })
