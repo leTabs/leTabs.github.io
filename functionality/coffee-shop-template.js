13a2c88fd1ac3373fe772bca5e4845a1bd3e1a2d
leTabs <125308756+leTabs@users.noreply.github.com>
2024-06-19 17:53:15 +0300

    Update coffee-shop-template.js

--- functionality/coffee-shop-template.js
+++ functionality/coffee-shop-template.js
@@ -80,7 +80,7 @@ for(let i = 0; i < expandHideBtnArray.length; i++){
     })
 }
 
-expandAllBtn.addEventListener('click', ()=>{
+function expandAllFunc(){
     for(let i = 0; i < expandHideBtnArray.length; i++){
         imgFrameArray[i].style.height = '100%'
         imgFrameArray[i].style.padding = '2rem'
@@ -91,7 +91,10 @@ expandAllBtn.addEventListener('click', ()=>{
             templateImgArray[i].style.display = 'block'
         }, 250)
     }
-})
+}
+expandAllFunc()
+
+expandAllBtn.addEventListener('click', ()=>{expandAllFunc()})
 
 
 collapseAllBtn.addEventListener('click', ()=>{
